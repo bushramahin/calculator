39a5756acca5890c6b56f30ee7a98be12cd41d5a
bushramahin <101120359+bushramahin@users.noreply.github.com>
2023-09-11 22:31:41 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# calculator
+  Simple CLI Calculator
---
+++ src/calculator.ts
@@ -0,0 +1,36 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+let {num1 , num2 , operation} = await inquirer.prompt([
+    {
+        name : "num1",
+        type : "number",
+        message : "Enter First Number"
+    },
+    {
+        name : "num2",
+        type : "number",
+        message : "Enter Second number"
+    },
+    {
+        name : "operation",
+        type : "list",
+        choices : ["addition" , "subtraction" , "multiplication" , "division"]
+    }
+]);
+if(operation == "addition"){
+    let add = num1 +num2;
+    console.log("addition" , chalk.bgBlueBright(add));
+}
+else if (operation == "subtraction"){
+    let sub = num1 - num2;
+    console.log("subtraction" , chalk.bgCyan(sub));
+}
+else if(operation == "multiplication"){
+    let product = num1*num2;
+    console.log("multiplication" , chalk.bgGreen(product));
+}
+else{
+    let div = num1/num2;
+    console.log("division" , chalk.bgCyan(div));
+    
+}
